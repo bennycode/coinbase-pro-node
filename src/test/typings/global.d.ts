c6b8cbee235bce6f409e5ed129c91ca06aac7ea0
Benny Neugebauer <bn@bennyn.de>
2020-01-12 12:59:27 +0100

    update linter

--- src/test/typings/global.d.ts
+++ src/test/typings/global.d.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {CoinbasePro} from '../../CoinbasePro';
 
 declare global {
