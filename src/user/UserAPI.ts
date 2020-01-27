8a150fecb7d32b7b7cd39a8109985f665aaee26e
Benny Neugebauer <bn@bennyn.de>
2020-01-27 17:34:10 +0100

    feat: Request trailing volume (#29)

--- src/user/UserAPI.ts
+++ src/user/UserAPI.ts
@@ -4,6 +4,13 @@ export interface VerifiedUser {
   id: string;
 }
 
+export interface TrailingVolume {
+  exchange_volume: string;
+  product_id: string;
+  recorded_at: string;
+  volume: string;
+}
+
 export class UserAPI {
   static readonly URL: {USERS: string} = {
     USERS: `/users`,
@@ -15,6 +22,19 @@ export class UserAPI {
   async verifyAuthentication(): Promise<VerifiedUser> {
     const resource = [UserAPI.URL.USERS, 'self', 'verify'].join('/');
     const response = await this.apiClient.get<VerifiedUser>(resource);
+    return response.data;
+  }
+
+  /**
+   * This request will return your 30-day trailing volume for all products of the API key’s profile. This is a cached
+   * value that’s calculated every day at midnight UTC.
+   * @note This endpoint requires either the “view” or “trade” permission
+   * @returns {Promise<TrailingVolume>} Your 30-day trailing volume for all products of the API key’s profile
+   * @see https://docs.pro.coinbase.com/#trailing-volume
+   */
+  async getTrailingVolume(): Promise<TrailingVolume[]> {
+    const resource = `${UserAPI.URL.USERS}/self/trailing-volume`;
+    const response = await this.apiClient.get<TrailingVolume[]>(resource);
     return response.data;
   }
 }
