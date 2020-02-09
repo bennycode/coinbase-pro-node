10754003bf57f39511c141627534136c7d6ac5fb
Benny Neugebauer <bn@bennyn.de>
2020-02-09 10:37:39 +0100

    test: Check types on test run

--- src/start.ts
+++ src/start.ts
@@ -4,14 +4,12 @@ require('dotenv').config();
 
 const {COINBASE_PRO_SANDBOX_API_KEY, COINBASE_PRO_SANDBOX_API_SECRET, COINBASE_PRO_SANDBOX_PASSPHRASE} = process.env;
 
-const client = new CoinbasePro(
-  {
-    apiKey: COINBASE_PRO_SANDBOX_API_KEY!,
-    apiSecret: COINBASE_PRO_SANDBOX_API_SECRET!,
-    passphrase: COINBASE_PRO_SANDBOX_PASSPHRASE!,
-  },
-  true
-);
+const client = new CoinbasePro({
+  apiKey: COINBASE_PRO_SANDBOX_API_KEY!,
+  apiSecret: COINBASE_PRO_SANDBOX_API_SECRET!,
+  passphrase: COINBASE_PRO_SANDBOX_PASSPHRASE!,
+  useSandbox: true,
+});
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async (): Promise<void> => {
