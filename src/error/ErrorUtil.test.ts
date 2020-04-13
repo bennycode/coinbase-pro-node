383aa87b509ffe9e4c13dbf7301e633972751667
Benny Neugebauer <bn@bennyn.de>
2020-04-13 11:49:36 +0200

    Prettier

--- src/error/ErrorUtil.test.ts
+++ src/error/ErrorUtil.test.ts
@@ -4,10 +4,9 @@ import type {AxiosError} from 'axios';
 describe('ErrorUtil', () => {
   describe('inAirPlaneMode', () => {
     it('recognizes connection aborts', () => {
-      const error =
-        {
-          code: 'ECONNABORTED',
-        } as AxiosError;
+      const error = {
+        code: 'ECONNABORTED',
+      } as AxiosError;
 
       expect(inAirPlaneMode(error)).toBe(true);
     });
@@ -19,19 +18,17 @@ describe('ErrorUtil', () => {
 
       expect(gotRateLimited(someError)).toBe(false);
 
-      const someResponse =
-        {
-          response: {},
-        } as AxiosError;
+      const someResponse = {
+        response: {},
+      } as AxiosError;
 
       expect(gotRateLimited(someResponse)).toBe(false);
 
-      const rateLimitError =
-        {
-          response: {
-            status: 429,
-          },
-        } as AxiosError;
+      const rateLimitError = {
+        response: {
+          status: 429,
+        },
+      } as AxiosError;
 
       expect(gotRateLimited(rateLimitError)).toBe(true);
     });
@@ -47,28 +44,26 @@ describe('ErrorUtil', () => {
 
     it('falls back to the standard error message', () => {
       const message = 'Something went wrong.';
-      const error =
-        {
-          message,
-          response: {
-            data: {},
-          },
-        } as AxiosError;
+      const error = {
+        message,
+        response: {
+          data: {},
+        },
+      } as AxiosError;
 
       expect(getErrorMessage(error)).toBe(message);
     });
 
     it('parses the error message from a server response', () => {
       const message = 'Something went wrong.';
-      const error =
-        {
-          message,
-          response: {
-            data: {
-              message,
-            },
+      const error = {
+        message,
+        response: {
+          data: {
+            message,
           },
-        } as AxiosError;
+        },
+      } as AxiosError;
 
       expect(getErrorMessage(error)).toBe(message);
     });
