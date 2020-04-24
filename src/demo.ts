fd74123060102dc2e918da48aa2e095684349071
Benny Neugebauer <bn@bennyn.de>
2020-04-25 00:02:31 +0200

    fix: Use correct sandbox API key in demo

--- src/demo.ts
+++ src/demo.ts
@@ -9,18 +9,18 @@ if (process.env.USE_SANDBOX === 'true') {
   console.info("Using Coinbase Pro's public sandbox with API key...");
 
   client = new CoinbasePro({
-    apiKey: process.env.COINBASE_PRO_API_KEY!,
-    apiSecret: process.env.COINBASE_PRO_API_SECRET!,
-    passphrase: process.env.COINBASE_PRO_PASSPHRASE!,
+    apiKey: process.env.COINBASE_PRO_SANDBOX_API_KEY!,
+    apiSecret: process.env.COINBASE_PRO_SANDBOX_API_SECRET!,
+    passphrase: process.env.COINBASE_PRO_SANDBOX_PASSPHRASE!,
     useSandbox: true,
   });
 } else if (process.env.USE_SANDBOX === 'false') {
   console.info("Using Coinbase Pro's production environment with API key...");
 
   client = new CoinbasePro({
-    apiKey: process.env.COINBASE_PRO_SANDBOX_API_KEY!,
-    apiSecret: process.env.COINBASE_PRO_SANDBOX_API_SECRET!,
-    passphrase: process.env.COINBASE_PRO_SANDBOX_PASSPHRASE!,
+    apiKey: process.env.COINBASE_PRO_API_KEY!,
+    apiSecret: process.env.COINBASE_PRO_API_SECRET!,
+    passphrase: process.env.COINBASE_PRO_PASSPHRASE!,
     useSandbox: false,
   });
 } else {
