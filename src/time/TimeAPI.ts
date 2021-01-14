9c30afcff85f4623bef5b99e98abea9f44387fc7
Benny Neugebauer <bn@bennyn.de>
2021-01-14 23:37:52 +0100

    Update TimeAPI.ts

--- src/time/TimeAPI.ts
+++ src/time/TimeAPI.ts
@@ -12,19 +12,19 @@ export class TimeAPI {
     TIME: `/time`,
   };
 
-  constructor(private readonly baseURL: string) {}
+  constructor(private readonly baseURL: string) {
+  }
 
   /**
-   * Get the server time from Coinbase Pro API.
+   * Get the server time from Coinbase Pro API. It has been reported that sometimes the return value is a string:
+   * https://github.com/bennycode/coinbase-pro-node/issues/354
    *
    * @see https://docs.pro.coinbase.com/#time
    */
   async getTime(): Promise<TimeSkew | string> {
     const response = await axios.get<TimeSkew>(`${this.baseURL}${TimeAPI.URL.TIME}`, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json;charset=utf-8',
-      },
+      // This trick forces axios to set JSON headers
+      data: {},
     });
     return response.data;
   }
@@ -34,6 +34,7 @@ export class TimeAPI {
    */
   async getClockSkew(): Promise<number> {
     const time = await this.getTime();
+    /** @see https://github.com/bennycode/coinbase-pro-node/issues/354 */
     const epoch = (typeof time === 'string') ? parseFloat(time.match(/epoch":(.*)\./i)![1]) : time.epoch;
     const now = Date.now() / 1000;
     return epoch - now;
