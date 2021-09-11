ae19ef25cb8883001c72d225daa378cf3a8fb771
Ohkthx <11020006+Ohkthx@users.noreply.github.com>
2021-09-11 04:00:05 -0500

    Update ProductAPI.test.ts

--- src/product/ProductAPI.test.ts
+++ src/product/ProductAPI.test.ts
@@ -14,13 +14,13 @@ describe('ProductAPI', () => {
 
   describe('getProduct', () => {
     it('returns trading details for a specified product', async () => {
+      const productId = 'BTC-EUR';
       nock(global.REST_URL)
-        .get(ProductAPI.URL.PRODUCTS)
+        .get(`${ProductAPI.URL.PRODUCTS}/${productId}`)
         .query(true)
         .reply(
           200,
-          JSON.stringify([
-            {
+          JSON.stringify({
               base_currency: 'BTC',
               base_increment: '0.00000001',
               base_max_size: '200',
@@ -37,30 +37,11 @@ describe('ProductAPI', () => {
               quote_increment: '0.01',
               status: 'online',
               status_message: '',
-            },
-            {
-              base_currency: 'XRP',
-              base_increment: '1',
-              base_max_size: '500000',
-              base_min_size: '1',
-              cancel_only: false,
-              display_name: 'XRP/USD',
-              id: 'XRP-USD',
-              limit_only: false,
-              margin_enabled: false,
-              max_market_funds: '100000',
-              min_market_funds: '10',
-              post_only: false,
-              quote_currency: 'USD',
-              quote_increment: '0.0001',
-              status: 'online',
-              status_message: '',
-            },
-          ])
+            })
         );
 
-      const product = await global.client.rest.product.getProduct('XRP-USD');
-      expect(product!.display_name).toBe('XRP/USD');
+      const product = await global.client.rest.product.getProduct(productId);
+      expect(product!.display_name).toBe('BTC/USD');
     });
   });
 
